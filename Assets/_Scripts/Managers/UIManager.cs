79f3d47876113b8d8544cb4b9ed0c77d861a2701
Niek Melet <niek+git@meletstudios.com>
2024-10-30 15:43:23 +0100

    Add UI structures to UIManager
    Introduced new UI structures including UIButton, UISlider, and UIPanel in UIManager. These structures bundle associated UI components together for easier management and setup. Adjustments include modifying the UI Scene file to integrate the new UIManager prefab.

--- Assets/_Scripts/Managers/UIManager.cs
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -1,7 +1,43 @@
-﻿namespace AstroNut.Managers
+﻿using TMPro;
+
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace AstroNut.Managers
 {
+    #region UI Structures
+
+    [System.Serializable]
+    public struct UIButton
+    {
+        public Button button;
+        public TMP_Text text;
+    }
+
+    [System.Serializable]
+    public struct UISlider
+    {
+        public Slider slider;
+        public TMP_Text text;
+    }
+
+    [System.Serializable]
+    public struct UIPanel
+    {
+        public GameObject panel;
+        public TMP_Text header;
+        
+        // Optional buttons/sliders
+        [Tooltip("The UIButton container.")] public UIButton[] buttons;
+        [Tooltip("The UISlider container.")] public UISlider[] sliders;
+    }
+
+    #endregion
+    
     public class UIManager : Singleton<UIManager>
     {
-        
+        [Header("Main Menu UI Elements")]
+        [SerializeField] private UIPanel mainMenuPanel;
     }
 }
